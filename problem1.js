4de6dd7909bc26e2148ab0f08b7887c08f7e9cb3
Thomas Varney <thomasvarney723@gmail.com>
2015-06-17 20:55:32 -0500

    Changed function to one that takes an argument and is immediately invoked

--- problem1.js
+++ problem1.js
@@ -4,16 +4,16 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-function sumOfMultiples() {
+(function sumOfMultiplesOfThreeOrFiveLessThan(numb) {
     var counter = 0;
     var sumofMults = 0;
-    while(counter < 1000) {
+    while(counter < numb) {
         if(counter % 5 === 0 || counter % 3 === 0) {
             sumofMults = sumofMults + counter;
         }
         counter++;
     }
     return sumofMults;
-}
+})(1000);
 
 // Answer: 233168

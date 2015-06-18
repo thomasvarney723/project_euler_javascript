47d559450332bd3f1001f575311f964624de7765
Thomas Varney <thomasvarney723@gmail.com>
2015-06-18 13:59:20 -0500

    First implimentation that worked

--- problem7.js
+++ problem7.js
@@ -4,43 +4,42 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 */
 
-function getPrimeNumber(numb) {
-    while(numb < 10001) {
-        
-    }
-}
-
-function isPrime(numb) {
-    var currentDivisor = Math.floor(numb / 2);
-    while(currentDivisor > 1) {
-        
-        if(numb % currentDivisor === 0) {
-            currentDivisor--;
+(function getPrimeNumber(numb) {
+    var countOfPrimes = [2];
+    var candidate = 3;
+    var divisor = Math.floor(candidate / 2);
+    while(countOfPrimes.length < numb) {
+        if(divisor > 1 && candidate % divisor !== 0) {
+            divisor--;
+        } else if(divisor === 1){
+            countOfPrimes.push(candidate);
+            candidate = candidate + 2;
+            divisor = Math.floor(candidate / 2);
         } else {
-            return false;
+            candidate = candidate + 2;
+            divisor = Math.floor(candidate / 2)
         }
-    }
-    return true;
-}
-
-function roundDown(numb) {
-    return Math.floor(numb / 2);
-}
+    } 
+    return countOfPrimes[countOfPrimes.length - 1];
+})(10001);
 
 /*
 Variable Table:
 
-number  divsor  countofPrimes
+candi   divsor  countofPrimes
 3       1       2
-5       2       2
-5       1       3
-7       3       3
-7       2       3
-7       1       4
-9       4       4
-9       3       4
-11      5       4
-11      4       4
-11      3       4
-11      2       4
-11      1       5
+5       2       2, 3
+5       1       2, 3
+7       3       2, 3, 5
+7       2       2, 3, 5
+7       1       2, 3, 5, 7
+9       4       2, 3, 5, 7
+9       3       2, 3, 5, 7
+11      5       2, 3, 5, 7
+11      4       2, 3, 5, 7
+11      3       2, 3, 5, 7
+11      2       2, 3, 5, 7
+11      1       2, 3, 5, 7, 11
+*/
+
+// Answer: 104743

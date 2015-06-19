1818bdb8171674f3c82589803dd75acf9f5fd658
Thomas Varney <thomasvarney723@gmail.com>
2015-06-19 18:35:52 -0500

    Changed i and j limits, added comment.

--- problem4.js
+++ problem4.js
@@ -6,8 +6,8 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 (function biggestPalendromeOfProductsUnder(numb){
     var palArray = [];
-    for(var i = numb; i > (numb / 9); i--) {
-        for(var j = numb; j > (numb / 9); j--) {
+    for(var i = numb; i > (numb / 2); i--) {
+        for(var j = numb; j > 0; j--) {
             if(isPalendrome(j * i)) {
             palArray.push(j * i);
             }
@@ -35,5 +35,9 @@ function reverse(a) {
     }
     return reversed;
 }
+
+/* Limiting i and j within the for loop to half of numb and zero respectively, 
+   shortens evaluation time a bit while still working for numb = 10 and numb = 1
+   The closer to the value of numb i and j are, the faster the evaluation time.*/
 
 // Answer: 906609

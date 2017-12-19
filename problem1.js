82403b0f04e3ebd02055e6f87ab4f58accba8c7a
Thomas Varney <thomasvarney723@gmail.com>
2017-12-18 23:12:54 -0600

    added re-write

--- problem1.js
+++ problem1.js
@@ -17,3 +17,25 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 })(1000);
 
 // Answer: 233168
+
+// original (above) on Jun 17 2015
+// re-write (below) on Dec 18 2017
+// essentially the same imperative code
+// the functional version turned out to be fast too
+
+function answer() {
+    var current = 0;
+    var accumulator = 0;
+    while (current < 1000) {
+	if (current % 3 === 0 || current % 5 === 0)
+	    accumulator += current;
+	current++;
+    }
+    return accumulator;
+}
+
+function answer2() {
+    return [...Array(1000).keys()]  // in lieu of a range fn
+        .filter(x => x % 3 === 0 || x % 5 === 0)
+        .reduce((a, b) => a + b, 0);
+}

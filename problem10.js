1d92d77815717a75813e91abfd061b89813c20fc
Thomas Varney <thomasvarney723@gmail.com>
2017-12-27 13:48:36 -0600

    added problem 10 're-write', basically copy pasted from my primes-numbers repo

--- problem10.js
+++ problem10.js
@@ -27,3 +27,30 @@ function isPrime(number) {
 This solution took over a minute to finish running on my chromebook with Node.js
 Maybe in the future I'll try Sieve of Eratosthenes
 */
+
+
+// basically copy-pasted from my prime-numbers repo on Dec 27 2017
+// approx 2000x faster than original
+
+function eratosthenes(n) {
+    var intArr = Array(n);  // array of 'undefined's of length n
+
+    intArr[0] = intArr[1] = false;  // 0 and 1 are not primes
+
+    var current = 2;
+    while(current < Math.sqrt(n)) {
+	for(var i = current * current; i < n; i += current)
+	    intArr[i] = false;  // eliminate multiples of current
+	current++;
+	while(intArr[current] === false)  // get the next prime
+	    current++;
+    }
+
+    var primes = [2];  // collect the primes
+    for(var j = 3; j < intArr.length; j += 2) {
+	if(intArr[j] === undefined)
+	    primes.push(j);
+    }
+
+    return primes.reduce((a, b) => a + b);
+}
